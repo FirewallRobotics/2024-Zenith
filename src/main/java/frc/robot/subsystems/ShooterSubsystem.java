84ecd5025755869f510444f03207a564e7c6edf3
Alex Swindell <alxswindell@gmail.com>
2024-01-27 16:46:20 -0500

    Shooter subsystem has motors

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -4,11 +4,37 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
-  public ShooterSubsystem() {}
+
+  public static CANSparkMax MasterShooterMotor;
+  public static CANSparkMax MinionShooterMotor;
+  public static AbsoluteEncoder ArmEncoder;
+
+  public ShooterSubsystem() {
+    MasterShooterMotor = new CANSparkMax(ShooterConstants.kMasterShooterMotorPort, MotorType.kBrushless);
+    MinionShooterMotor = new CANSparkMax(ShooterConstants.kMinionShooterMotorPort, MotorType.kBrushless);
+
+    MasterShooterMotor.restoreFactoryDefaults();
+    MinionShooterMotor.restoreFactoryDefaults();
+
+    MasterShooterMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+    MasterShooterMotor.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+
+    MasterShooterMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 10);
+    MasterShooterMotor.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 0);
+
+    // MasterIntakeMotor.setIdleMode(IdleMode.kCoast);
+
+    MinionShooterMotor.follow(MasterShooterMotor, true);
+  }
 
   @Override
   public void periodic() {
@@ -16,14 +42,18 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public void ShootAmp(){
-
+    MasterShooterMotor.set(ShooterConstants.kShooterMotorSpeed);
   }
 
   public void ShootSpeaker(){
-
+    MasterShooterMotor.set(ShooterConstants.kShooterMotorSpeed);
   }
 
   public void ShootTrap(){
-    
+    MasterShooterMotor.set(ShooterConstants.kShooterMotorSpeed);
+  }
+
+  public void StopShoot(){
+    MasterShooterMotor.set(0);
   }
 }
