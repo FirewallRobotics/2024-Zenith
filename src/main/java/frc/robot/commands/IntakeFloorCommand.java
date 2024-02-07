4ce68aa9dfacc42f194dfaacfef74082fa71710d
Cpixel151 <pixeldigit151@gmail.com>
2024-02-07 18:59:54 -0500

    Added functions to the floor Intake command

--- src/main/java/frc/robot/commands/IntakeFloorCommand.java
+++ src/main/java/frc/robot/commands/IntakeFloorCommand.java
@@ -30,15 +30,27 @@ public class IntakeFloorCommand extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_Seesaw.IntakeFloorAngle();
+
+    // This is the LED sensor.
+    m_Intake.sensorStartIntake();
+
+    m_Intake.StartIntake();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    if (interrupted == true) {
+      m_Intake.StopIntake();
+    }
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    m_Intake.StopIntake();
     return false;
   }
 }
