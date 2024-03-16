10ef22d5fbdf8098fcecfec85dd7f16d28a3c323
NoobinOut <bigboi@duck.com>
2024-03-16 11:56:54 -0400

    Update FinalPiProgram.py

--- src/orangepi/ProductionFiles/FinalPiProgram.py
+++ src/orangepi/ProductionFiles/FinalPiProgram.py
@@ -297,39 +297,39 @@ def startSwitchedCamera(config):
         listener)
 
     return server
+if testmode == False:
+    if __name__ == "__main__":
+        if len(sys.argv) >= 2:
+            configFile = sys.argv[1]
 
-if __name__ == "__main__":
-    if len(sys.argv) >= 2:
-        configFile = sys.argv[1]
+        # read configuration
+        if not readConfig():
+            sys.exit(1)
 
-    # read configuration
-    if not readConfig():
-        sys.exit(1)
+        # start NetworkTables
+        ntinst = NetworkTableInstance.getDefault()
+        if server:
+            print("Setting up NetworkTables server")
+            ntinst.startServer()
+        else:
+            print("Setting up NetworkTables client for team {}".format(team))
+            ntinst.startClient4("wpilibpi")
+            ntinst.setServerTeam(team)
+            ntinst.startDSClient()
 
-    # start NetworkTables
-    ntinst = NetworkTableInstance.getDefault()
-    if server:
-        print("Setting up NetworkTables server")
-        ntinst.startServer()
-    else:
-        print("Setting up NetworkTables client for team {}".format(team))
-        ntinst.startClient4("wpilibpi")
-        ntinst.setServerTeam(team)
-        ntinst.startDSClient()
+        # start cameras
+        # work around wpilibsuite/allwpilib#5055
+        CameraServer.setSize(CameraServer.kSize160x120)
+        for config in cameraConfigs:
+            cameras.append(startCamera(config))
 
-    # start cameras
-    # work around wpilibsuite/allwpilib#5055
-    CameraServer.setSize(CameraServer.kSize160x120)
-    for config in cameraConfigs:
-        cameras.append(startCamera(config))
-
-    # start switched cameras
-    for config in switchedCameraConfigs:
-        startSwitchedCamera(config)
-    # get frame/sink to process from first camera
-    img = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
-    cvSink = CameraServer.getVideo()
-    output = CameraServer.putVideo("AprilTags", 680, 360)
+        # start switched cameras
+        for config in switchedCameraConfigs:
+            startSwitchedCamera(config)
+        # get frame/sink to process from first camera
+        img = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
+        cvSink = CameraServer.getVideo()
+        output = CameraServer.putVideo("AprilTags", 680, 360)
 
 
 
