75e00938c0f5b923b44acc96258b8959a6da26fe
Alex Swindell <alxswindell@gmail.com>
2024-03-24 13:16:26 -0400

    9 second wait on side autos

--- src/main/java/frc/robot/subsystems/AutonomousTrajectories.java
+++ src/main/java/frc/robot/subsystems/AutonomousTrajectories.java
@@ -667,6 +667,7 @@ public class AutonomousTrajectories extends SubsystemBase {
     return new SequentialCommandGroup(
         new AutoBasicAimSpeakerCommand(m_axle, m_climb),
         getShootCommandWithTimeout(m_shooter, m_axle, m_intake),
+        new WaitCommand(9),
         getTrajectoryCommand(getShortForwardTrajectory(trajectoryConfig), thetaController),
         getTrajectoryCommand(
             getDiagonalTrajectory(trajectoryConfig, true, m_robotDrive.getPose()),
@@ -677,6 +678,7 @@ public class AutonomousTrajectories extends SubsystemBase {
     return new SequentialCommandGroup(
         new AutoBasicAimSpeakerCommand(m_axle, m_climb),
         getShootCommandWithTimeout(m_shooter, m_axle, m_intake),
+        new WaitCommand(9),
         getTrajectoryCommand(getForwardTrajectory(trajectoryConfig), thetaController),
         getTrajectoryCommand(
             getDiagonalTrajectory(trajectoryConfig, false, m_robotDrive.getPose()),
@@ -687,6 +689,7 @@ public class AutonomousTrajectories extends SubsystemBase {
     return new SequentialCommandGroup(
         new AutoBasicAimSpeakerCommand(m_axle, m_climb),
         getShootCommandWithTimeout(m_shooter, m_axle, m_intake),
+        new WaitCommand(9),
         getTrajectoryCommand(getForwardTrajectory(trajectoryConfig), thetaController),
         getTrajectoryCommand(
             getDiagonalTrajectory(trajectoryConfig, true, m_robotDrive.getPose()),
@@ -697,6 +700,7 @@ public class AutonomousTrajectories extends SubsystemBase {
     return new SequentialCommandGroup(
         new AutoBasicAimSpeakerCommand(m_axle, m_climb),
         getShootCommandWithTimeout(m_shooter, m_axle, m_intake),
+        new WaitCommand(9),
         getTrajectoryCommand(getShortForwardTrajectory(trajectoryConfig), thetaController),
         getTrajectoryCommand(
             getDiagonalTrajectory(trajectoryConfig, false, m_robotDrive.getPose()),
